1b859ea3f6a63c18ddc0748afcb0886f12d23668
Victor Rodrigues <vrf1130@gmail.com>
2022-08-17 18:28:05 -0300

    primeiro programa com java

---
+++ src/Exercicio.java
@@ -0,0 +1,8 @@
+public class Exercicio{
+	public static void main(String args[]) {
+		String nome = "victor"; 
+		System.out.println("Meu nome é "+nome);
+		System.out.println("Programa em java");
+		System.out.println("Até o proximo programa");
+	}
+}
